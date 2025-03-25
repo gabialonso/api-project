5f87102588ec2465eeb4bb3b0e5f57669f49b6a7
gabialonso <galonso@ingsw.com>
2025-03-25 14:41:57 -0300

    Add script.js to fetch Giphy API

--- script.js
+++ script.js
@@ -3,12 +3,29 @@ const apiKey = "t2y24a7WChxP8UybZCzTWHnNGdd7hc3w";
 document.getElementById("searchButton").addEventListener("click", fetchGifs);
 
 function fetchGifs() {
-    const query = document.getElementById("searchInput").value;
-    if (query === "") return;
+    const query = document.getElementById("gifInput").value;
+    if (query === "") {
+        alert("Please enter a search term");
+        return;
+    }
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=3`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            displayGifs(data);
+        })
+        .catch(error => console.error("Error fetching data:", error));
 }
 
-const url = `api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=10`;
+function displayGifs(data) {
+    const gifContainer = document.getElementById("gifContainer");
+    gifContainer.innerHTML = "";
 
-fetch(url)
-    .then(response => response.json())
-    .then()
+    data.data.forEach(gif => {
+        const img = document.createElement("img");
+        img.src = gif.images.original.url;
+        gifContainer.appendChild(img);
+    });
+}
